8eb153ea25a24d935dffb44edb544af3c24c8642
Zujiry <Zujiry@users.noreply.github.com>
2014-11-01 15:10:28 +0100

    Habs jetzt...
    Iwie ist die Auto Typprädikatsfunktion kacke

--- Aufgabe3/lib/day_class.rb
+++ Aufgabe3/lib/day_class.rb
@@ -10,20 +10,13 @@ require 'ext_pr1_v4'
 # Day ::= (DayNum | DaySym) ::
 #
 
-def_class(:DAY_NUM,[:num])
-def_class(:DAY_SYM,[:sym])
+def_class(:Daynum,[:num])
+def_class(:Daysym,[:sym])
 
-DayNum = DAY_NUM[(1..7)]
-DaySym = DAY_SYM[[:Mo, :Di, :Mi, :Do, :Fr, :Sa, :So]]
+Daynum[(1..7)]
+Daysym[[:Mo, :Di, :Mi, :Do, :Fr, :Sa, :So]]
 
-def day_num?(any)       # => brauchen hier obj.day_num? aber weiß nicht wie...
-  any.int? and DayNum.num.include?(any)
-end
-
-def day_sym?(any)
-  any.symbol? and DaySym.sym.include?(any)
-end
 
 def day?(any)
-  day_num?(any) or day_sym?(any)
+  any.daynum? or any.daysym?
 end
