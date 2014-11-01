cf76e698ee629f1ffadda62de0717edcc3e21abc
Zujiry <Zujiry@users.noreply.github.com>
2014-11-01 15:19:41 +0100

    Everything is true

--- Aufgabe3/test/day_class_test.rb
+++ Aufgabe3/test/day_class_test.rb
@@ -10,5 +10,12 @@ require 'day_class'
 class DayTest < Test::Unit::TestCase
   def test_day?
     assert_equal(true,day?(Daysym[:Mo]))
+    assert_equal(true,day?(Daysym[:So]))
+    assert_equal(false,day?(Daysym["Do"]))
+    assert_equal(false,day?(Daysym[1]))
+    assert_equal(true,day?(Daynum[1]))
+    assert_equal(true,day?(Daynum[7]))
+    assert_equal(false,day?(Daynum[:Mo]))
+    assert_equal(false,day?(Daynum["1"]))
   end
 end
