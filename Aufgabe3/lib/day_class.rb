c5f760962ceed209990d4a2f13e10295cb5ee4d6
Zujiry <Zujiry@users.noreply.github.com>
2014-11-04 23:16:44 +0100

    Alles iwie nicht so schön

--- Aufgabe3/lib/day_class.rb
+++ Aufgabe3/lib/day_class.rb
@@ -35,10 +35,47 @@ end
 
 
 def to_day(typ,var)
-  typ.day_num? or typ.day_sym?
-  var 
+  day?(num_sym(typ)) and var.int? or var.symbol?
+  if    num_sym(typ).day_num?
+    var.int? ? var : day_sym_to_day_num(var) 
+  elsif num_sym(typ).day_sym?
+    num_sym(var).day_sym? ? var : day_num_to_day_sym(var)
+  end
 end
 
-def day_shift(var,int1)
-  
+#day_shift ::= Day x Int->Day::Test{(DaySym[:Mo],-2) => DaySym[:Sa]) 
+def day_shift(day,int)
+  day.day_sym? and int.int?
+  DaySym[DAY_SYM[(((DAY_SYM.index(day.sym)))+int)%DAY_SYM.size]]
+ # to_day((day_sym_to_day_num(day).num + int)%DAY_SYM.size)
 end
+
+#Convertingfunction for nicer input
+#num_sym ::= Int x Symbol -> DayNum[Int] or DaySyn[Symbol] ::
+def num_sym(var)
+  if var.int?
+    DayNum[var]
+  elsif var.symbol?
+    DaySym[var]
+  end
+end
+
+# Conversion-functions day_num_to_day_sym and day_sym_to_day_num
+# day_num_to_day_sym :: DayNum -> DaySym ::
+# (day_num) :::: day_num -> day_sym ::
+# Tests: {[1] => :Mo; [7] => :So; [8] => Err; ["Fr"] => Err; [:Mo] => Err;}
+def day_num_to_day_sym(day)
+  num_sym(day).day_num?
+  DAY_SYM[day-1]
+end
+
+# day_num_to_day_sym :: DaySym -> DayNum ::
+# (day_sym) :::: day_sym -> day_num ::
+# Tests: {[:Di] => 2; [:Sa] => 6; [:Ab] => Err; ["Fr"] => Err; [3] => Err;}
+def day_sym_to_day_num(day)
+  num_sym(day).day_sym?
+  DAY_NUM[(DAY_SYM.index(day))%DAY_SYM.size]
+end
+
+
+
