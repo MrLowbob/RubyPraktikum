5198cf11c0dfed133f5ad7412c4553053851eb64
Zujiry <Zujiry@users.noreply.github.com>
2014-11-06 12:55:43 +0100

    Aufg3 Shape include?

--- Aufgabe3/lib/dimension_functions.rb
+++ Aufgabe3/lib/dimension_functions.rb
@@ -1,3 +1,4 @@
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
 require 'eins_d'
 require 'zwei_d'
 require 'n_d'
@@ -8,5 +9,18 @@ require 'ext_pr1_v4'
 
 #shape_include? ::= (shape,point) :: Shape x Point->?Bool
 def shape_include?(shape, point)
+  if      shape.union1d? or range1d?(shape) and point.int?
+    then  shape1d_include?(shape, point)
+  elsif   shape.union2d? and point.point2d?
+    then  shape2d_include?(shape, point)
+  else check_pre((false))
+  end
+end
+
+def shape1d_include?(shape, point)
+  
+end
+
+def shape2d_include?(shape, point)
   
 end
