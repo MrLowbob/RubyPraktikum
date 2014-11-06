1e7e4754a0447083f1d217e49be3f9e5b2383d19
Zujiry <Zujiry@users.noreply.github.com>
2014-11-06 12:41:02 +0100

    Aufg3 Reup

--- Aufgabe3/lib/dimension_functions.rb
+++ Aufgabe3/lib/dimension_functions.rb
@@ -1,7 +1,12 @@
+require 'eins_d'
+require 'zwei_d'
+require 'n_d'
+
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__),'../..','ext_pr1/lib')
 require 'ext_pr1_v4'
-require '1D'
-require '2D'
-require 'nD'
 
 
+#shape_include? ::= (shape,point) :: Shape x Point->?Bool
+def shape_include?(shape, point)
+  
+end
---
+++ Aufgabe3/lib/n_d.rb
@@ -0,0 +1,68 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__),'../..','ext_pr1/lib')
+require 'ext_pr1_v4'
+
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+require 'eins_d'
+require 'zwei_d'
+
+#Point ::= Point1d | Point2d
+def_class(:Point, [:point]) {
+  def invariant?
+    point1d?(point) or point2d?(point)
+  end
+}
+
+#PrimShape ::= Range1d | Range2d
+def_class(:PrimShape, [:range]) {
+  def invariant?
+    range1d?(range) or range2d?(range)
+  end
+}
+
+#UnionShape::= Union1d | Union2d
+def_class(:UnionShape, [:union]) {
+  def invariant?
+    union1d?(union) or union2d?(union)
+  end
+}
+
+#CompShape::= UnionShape
+def_class(:CompShape, [:unionshape]) {
+  def invariant?
+    unionshape?(unionshape)
+  end
+}
+
+#Shape::= PrimShape|CompShape
+def_class(:Shape, [:pcshape]) {
+  def invariant?
+    primshape?(pcshape) or compshape?(pcshape)
+  end
+}
+
+#GraphObj::= Point | Shape
+def_class(:GraphObj, [:poshap]) {
+  def invariant?
+    point?(poshap) or shape?(poshap)
+  end
+}
+
+def unionshape?(o)
+  o.union_shape?
+end
+
+def primshape?(o)
+  o.prim_shape?
+end
+
+def comshape?(o)
+  o.comp_shape?
+end
+
+def point?(o)
+  o.point?
+end
+
+def shape?(o)
+  o.shape?
+end
