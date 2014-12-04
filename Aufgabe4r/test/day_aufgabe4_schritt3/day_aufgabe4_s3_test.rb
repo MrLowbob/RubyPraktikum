423d1e599b7eaa797bddbb2efadd1572e603501c
Zujiry <Zujiry@users.noreply.github.com>
2014-12-04 15:13:30 +0100

    Deleting Unused Projects

--- Aufgabe4r/test/day_aufgabe4_schritt3/day_aufgabe4_s3_test.rb
+++
@@ -1,7 +0,0 @@
-
-
-$:.unshift File.join(File.dirname(__FILE__),'../..','lib/day_aufgabe4_schritt3')
-require 'day_main'
-
-$:.unshift File.join(File.dirname(__FILE__),'../..','test/day_aufgabe4_schritt3')
-require 'day_aufgabe4'
